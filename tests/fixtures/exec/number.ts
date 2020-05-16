c7d8e76484be3ffa64bc352de554cf59cdaa5df9
Vedant Roy <vroy101@gmail.com>
2020-05-15 20:26:50 -0400

    Shorten comments

--- tests/fixtures/exec/number.ts
+++ tests/fixtures/exec/number.ts
@@ -5,17 +5,14 @@ import type { ExecutionContext } from "ava";
 export default (t: ExecutionContext) => {
   const numberV = createValidator<number>();
   tBV(t, numberV, {
-    // seems odd, but NaN and Infinity
-    // are both of type number in Typescript
-    // (and the typeof operator)
+    // NaN and Infinity are both numbers
+    // (according to the typeof operator,
+    // which Typescript uses for categorizing things)
     // This is why this file exists!
     input: [-1, 0, 1, NaN, Infinity],
     returns: true,
   });
   tBV(t, numberV, {
-    // seems odd, but NaN and Infinity
-    // are both of type number in Typescript
-    // (and the typeof operator)
     input: ["hello", null, {}],
     returns: false,
   });
