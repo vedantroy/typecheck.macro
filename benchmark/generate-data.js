188d18c06712dfc6d2ec3192eb956ebbe4af465e
Vedant Roy <vroy101@gmail.com>
2020-05-17 17:12:33 -0400

    Add progress indicator to benchmark data gen

--- benchmark/generate-data.js
+++ benchmark/generate-data.js
@@ -13,8 +13,14 @@ for (const schemaName of fse.readdirSync(schemasDir)) {
   );
   benchmarkNames.push(schemaName.slice(0, -".json".length));
   const data = [];
+  const numFakes = 5000000;
+  const onePercent = numFakes / 100;
+  let percentSoFar = 0;
   // 100000 (1 million -- seems to give accurate results)
-  for (let i = 0; i < 1000000; ++i) {
+  for (let i = 0; i < numFakes; ++i) {
+    if (i % onePercent === 0) {
+      console.log(`${++percentSoFar}%`);
+    }
     data.push(faker.generate(schema));
   }
   fse.outputJson(path.join(generatedDataDir, schemaName), data);
