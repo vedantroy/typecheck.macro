353ee79a683efb21f66ff5bb6c8d396228352c79
Vedant Roy <vroy101@gmail.com>
2020-05-20 22:05:44 -0400

    Add extra type resolution test

--- tests/ir/resolve.ts
+++ tests/ir/resolve.ts
@@ -19,10 +19,31 @@ test("resolve-chained", (t) => {
   t.snapshot(__dumpAfterTypeResolution("X", "Y", "Z"));
 });
 
+test("resolve-chained-complex", (t) => {
+  interface RCC5 {}
+  type RCC4 = RCC5;
+  type RCC3 = RCC4 | RCC5;
+  type RCC2 = RCC3 | RCC4;
+  type RCC1 = RCC2 | RCC3;
+  // ((RCC5 | RCC5) | RCC4) | (RCC5 | RCC5)
+  register("RCC1");
+  t.snapshot(__dumpAfterTypeResolution("RCC1"));
+});
+
 test("resolve-generics", (t) => {
   interface GI<P1, P2> {}
   type GY<P1, P2 = number> = GI<P1, P2>;
   type GZ<P1> = GY<P1>;
   register("GZ");
   t.snapshot(__dumpAfterTypeResolution("GI", "GY", "GZ"));
+});
+
+test("resolve-interface", (t) => {
+  type Baz = Foo;
+  type Bar = Baz;
+  interface Foo {
+    val: Bar;
+  }
+  register("Foo");
+  t.snapshot(__dumpAfterTypeResolution("Baz", "Bar", "Foo"));
 });
