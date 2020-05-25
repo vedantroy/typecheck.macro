f70012d671d575007f47659b9e9d22c1adca5b3a
Vedant Roy <vroy101@gmail.com>
2020-05-24 22:11:22 -0400

    Note potential change

--- src/type-ir/passes/resolve.ts
+++ src/type-ir/passes/resolve.ts
@@ -3,7 +3,7 @@ import { Errors, throwUnexpectedError } from "../../macro-assertions";
 import { IR, Type } from "../IR";
 import { traverse, applyTypeParameters } from "./utils";
 import deepCopy from "fast-copy";
-import { isType, isTypeAlias, isInterface, isBuiltinType } from "../IRUtils";
+import { isType, isTypeAlias, isInterface, isBuiltinType, isObjectPattern } from "../IRUtils";
 
 export interface ResolveState {
   readonly namedTypes: ReadonlyMap<string, IR>;
@@ -46,6 +46,13 @@ function resolveType(ir: IR, state: ResolveState): IR {
         typeName,
         typeParameters
       );
+      // POTENTIAL CHANGE: type aliases that refer to object patterns are essentially
+      // interfaces, so why not treat them as such?
+      /*
+      if (isObjectPattern(referencedIR.value)) {
+        return typeRef
+      }
+      */
       const newVisited = deepCopy(visitedTypes);
       newVisited.add(typeName);
       return resolveType(instantiatedIR, {
