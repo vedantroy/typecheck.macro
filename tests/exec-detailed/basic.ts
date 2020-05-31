8c7fedd0fccedc99a3f8da94dce45e7f85a6babc
Vedant Roy <vroy101@gmail.com>
2020-05-31 18:23:23 -0400

    More error message tests

---
+++ tests/exec-detailed/basic.ts
@@ -0,0 +1,18 @@
+import { createDetailedValidator, register } from "../../dist/typecheck.macro";
+import test from "ava";
+import * as u from "../../src/type-ir/IRUtils";
+
+test("literal", (t) => {
+  // TODO: Incomplete test
+  const x = createDetailedValidator<"Hello">();
+  let errs = [];
+  t.true(x("Hello", errs));
+  t.deepEqual(errs, []);
+
+  t.false(x(null, errs));
+  const helloLiteral = u.Literal("Hello");
+  t.deepEqual(errs, [["input", null, helloLiteral]]);
+  errs = [];
+  t.false(x("hello2", errs));
+  t.deepEqual(errs, [["input", "hello2", helloLiteral]]);
+});
