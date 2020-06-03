c29e58f85ecb3a4da7cca27361f79af1cf20fc93
Vedant Roy <vroy101@gmail.com>
2020-06-02 20:43:18 -0400

    Refactor data generation

--- benchmark/generate-data.js
+++ benchmark/generate-data.js
@@ -1,27 +1,23 @@
 const faker = require("json-schema-faker");
 const fse = require("fs-extra");
 const path = require("path");
+const argv = require("minimist")(process.argv.slice(2));
 
 const schemasDir = path.join(__dirname, "schemas");
 const generatedDataDir = path.join(__dirname, "generated");
 
-const benchmarkNames = [];
+const schemaName = path.join(schemasDir, argv._[0] + '.json')
 
-for (const schemaName of fse.readdirSync(schemasDir)) {
-  const schema = JSON.parse(
-    fse.readFileSync(path.join(schemasDir, schemaName))
-  );
-  benchmarkNames.push(schemaName.slice(0, -".json".length));
-  const data = [];
-  const numFakes = 5000000; //5000000;
-  const onePercent = numFakes / 100;
-  let percentSoFar = 0;
-  // 100000 (1 million -- seems to give accurate results)
-  for (let i = 0; i < numFakes; ++i) {
-    if (i % onePercent === 0) {
-      console.log(`${++percentSoFar}%`);
-    }
-    data.push(faker.generate(schema));
+const schema = JSON.parse(fse.readFileSync(schemaName));
+const data = [];
+const numFakes = 1000000; //5000000;
+const onePercent = numFakes / 100;
+let percentSoFar = 0;
+// 100000 (1 million -- seems to give accurate results)
+for (let i = 0; i < numFakes; ++i) {
+  if (i % onePercent === 0) {
+    console.log(`${++percentSoFar}%`);
   }
-  fse.outputJson(path.join(generatedDataDir, schemaName), data);
+  data.push(faker.generate(schema));
 }
+fse.outputJson(path.join(generatedDataDir, schemaName), data);
