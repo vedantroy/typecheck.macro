cdb9cd691eaef79a9c9a5bbc5770e4dd391cd277
Vedant Roy <vroy101@gmail.com>
2020-06-06 01:33:37 -0400

    Release new version that validates maps/sets

--- example/src/complex.ts
+++ example/src/complex.ts
@@ -1,40 +1,51 @@
-import { registerType, createDetailedValidator } from "typecheck.macro"
+import createValidator, {
+  registerType,
+  createDetailedValidator,
+} from "typecheck.macro";
 
 // We can validate circular data
-type LinkedList<T> = T & {next: LinkedList<T>} | null
-registerType('LinkedList')
+type LinkedList<T> = (T & { next: LinkedList<T> }) | null;
+registerType("LinkedList");
 
 interface Anime {
-    name: string
-    rating: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8| 9 | 10
+  name: string;
+  rating: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 }
-registerType('Anime')
+registerType("Anime");
 
-const isAnimeLinkedList = createDetailedValidator<LinkedList<Anime>>()
+const isAnimeLinkedList = createDetailedValidator<LinkedList<Anime>>();
 
-const A: LinkedList<Anime> = {next: null, name: 'Spirited Away', rating: 6}
-const B: LinkedList<Anime> = {next: null, name: '5 Centimeters per Second', rating: 5}
-const C: LinkedList<Anime> = {next: null, name: 'Ping Pong the Animation', rating: 10}
+const A: LinkedList<Anime> = { next: null, name: "Spirited Away", rating: 6 };
+const B: LinkedList<Anime> = {
+  next: null,
+  name: "5 Centimeters per Second",
+  rating: 5,
+};
+const C: LinkedList<Anime> = {
+  next: null,
+  name: "Ping Pong the Animation",
+  rating: 10,
+};
 A.next = B;
 B.next = C;
-C.next = B
+C.next = B;
 
-let errs = []
-console.log(isAnimeLinkedList(A, errs)) // true
-console.log(isAnimeLinkedList(B, errs)) // true
-console.log(isAnimeLinkedList(C, errs)) // true
+let errs = [];
+console.log(isAnimeLinkedList(A, errs)); // true
+console.log(isAnimeLinkedList(B, errs)); // true
+console.log(isAnimeLinkedList(C, errs)); // true
 
 // @ts-ignore
-B.name = 666
+B.name = 666;
 
 // all are false because if a single node in a LinkedList is not valid
 // then the entire LinkedList is not valid
-console.log(isAnimeLinkedList(A, errs))
-errs = []
-console.log(isAnimeLinkedList(B, errs))
-errs = []
-console.log(isAnimeLinkedList(C, errs))
-console.log(errs)
+console.log(isAnimeLinkedList(A, errs));
+errs = [];
+console.log(isAnimeLinkedList(B, errs));
+errs = [];
+console.log(isAnimeLinkedList(C, errs));
+console.log(errs);
 
 // Demonstration: error messages
 // Pass in an array
@@ -46,25 +57,50 @@ console.log(errs)
 // to find all possible errors
 
 interface GoodMusicArtist {
-    name: "Lil Uzi Vert";
-    albums: {[albumName: string]: string[]}
+  name: "Lil Uzi Vert";
+  albums: { [albumName: string]: string[] };
 }
-registerType('GoodMusicArtist')
+registerType("GoodMusicArtist");
 
-const isGoodMusicArtist = createDetailedValidator<GoodMusicArtist>()
-errs = []
-console.log(isGoodMusicArtist({
-    name: "Lil Uzi Vert",
-    // not gonna list out all the songs
-    albums: {"Eternal Atake": ["Baby Pluto", "Lo Mein"]}
-}, errs)) // true
+const isGoodMusicArtist = createDetailedValidator<GoodMusicArtist>();
+errs = [];
+console.log(
+  isGoodMusicArtist(
+    {
+      name: "Lil Uzi Vert",
+      albums: { "Eternal Atake": ["Baby Pluto", "Lo Mein"] },
+    },
+    errs
+  )
+); // true
 
-// Sorry, Queen.
-console.log(isGoodMusicArtist({
-    name: "Queen",
-    // does anyone even know their other songs?
-    albums: {"A Night at the Opera": ["Bohemian Rhapsody", 13]}
-}, errs)) // false
-console.log(errs)
+console.log(
+  isGoodMusicArtist(
+    {
+      name: "Queen",
+      albums: { "A Night at the Opera": ["Bohemian Rhapsody", 13] },
+    },
+    errs
+  )
+); // false
+console.log(errs);
+
+// In addition to arrays, the other native JS data structures are supported: Set and Map
+interface SetAndMap {
+  set: Set<number | Set<number>>;
+  map: Map<Map<number, string>, string>;
+}
+registerType("SetAndMap");
+const setAndMapValidator = createValidator<SetAndMap>();
+const valid = {
+  set: new Set([3, new Set([3])]),
+  map: new Map([[new Map([[3, "hey"]]), ""]]),
+};
+const invalid = {
+  set: new Set([3, new Set([3])]),
+  map: new Map([[new Map([[3, 4]]), ""]]),
+};
+console.log(setAndMapValidator(valid)); // true
+console.log(setAndMapValidator(invalid)); // false
 
 // To see all the crazy things this library can do, check out the tests (particularly, the exec tests)
